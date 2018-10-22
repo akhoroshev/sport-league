2362f57a4b9a265971616fe281cab01976dbeff5
nikolay-izumov <nikolay.izumov@gmail.com>
2018-10-22 16:28:04 +0300

    add flask hello world

---
+++ backend/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+flask hello world
+> FLASK_APP=hello.py flask run 
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello World!"
