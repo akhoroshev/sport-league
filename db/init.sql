441c2f1211419eb2115094730bdc32a7d286bfe6
Nikolay <nikolay.izumov@gmail.com>
2018-10-24 08:13:27 +0300

    Add init db data script

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+INSERT INTO sports VALUES (default, 'Poker', 'SNG 6-MAX');
+INSERT INTO sports VALUES (default, 'Dota 2', '5x5');
+INSERT INTO sports VALUES (default, 'Tennis', 'Ping-Pong');
+
+INSERT INTO places VALUES (default, 'Times', 'JB Home', 59.980907, 30.324603);
+INSERT INTO places VALUES (default, 'Stadium', 'Common Stadium', 60.002804, 30.371515);
+INSERT INTO places VALUES (default, 'Park', 'Park Sosnovka', 60.013269, 30.347687);
